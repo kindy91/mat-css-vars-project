fdddcca71bb7bbb2fe594ba3ce0999b3c12016a0
Abdullah <abdullah.kindy@gmail.com>
2024-03-13 20:24:45 +0100

    remaining files commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideMaterialCssVars } from "angular-material-css-vars";
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes), 
+    provideAnimationsAsync(),
+    provideMaterialCssVars({
+      isAutoContrast: true,
+      primary: "#3f51b5",
+    }),
+  ]
 };
